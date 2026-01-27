714a30dfc910cada5d0e5c97146d4f2079108ce2
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-01-27 13:53:08 +0100

    Ejercicio 2 basicos Objetos Arrays acabado

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,16 @@ public class Main {
         Alumno[] alumnos = new Alumno[5];
 
         alumnos[0] = new Alumno("Carlos", 10);
-        alumnos[1] = new Alumno("Iván", 8);
-        alumnos[2] = new Alumno("Iker", 9);
+        alumnos[1] = new Alumno("Iván", 4);
+        alumnos[2] = new Alumno("Iker", 8);
         alumnos[3] = new Alumno("Dani", 7);
-        alumnos[4] = new Alumno("Isabella", 6);
+        alumnos[4] = new Alumno("Isabella", 3);
 
         for (int i = 0; i < alumnos.length; i++) {
-            alumnos[i].mostrarDatos();
+            if (alumnos[i].getNota() >= 5) {
+                alumnos[i].mostrarDatos();
+            }
+
         }
     }
 }
