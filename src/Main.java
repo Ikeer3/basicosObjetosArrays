f9186e1e47014722dc5faf1407b732992312e54e
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-01-27 10:12:28 +0100

    Creado array alumnos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        Alumno[] alumnos = new Alumno[5];
+
+        alumnos[0] = new Alumno("Carlos", 10);
+        alumnos[1] = new Alumno("Iván", 8);
+        alumnos[2] = new Alumno("Iker", 9);
+        alumnos[3] = new Alumno("Dani", 7);
+        alumnos[4] = new Alumno("Isabella", 6);
     }
 }
