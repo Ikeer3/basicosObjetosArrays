e73387581a124f6730825b7f16484ec05c898fbd
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-01-29 12:33:12 +0100

    Implementada la media y el mejor alumno

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,28 @@ public class Main {
 
         Alumno[] alumnos = new Alumno[5];
 
-        alumnos[0] = new Alumno("Carlos", 10);
+        alumnos[0] = new Alumno("Carlos", 1);
         alumnos[1] = new Alumno("Iván", 4);
         alumnos[2] = new Alumno("Iker", 8);
         alumnos[3] = new Alumno("Dani", 7);
         alumnos[4] = new Alumno("Isabella", 3);
 
-        for (int i = 0; i < alumnos.length; i++) {
-            if (alumnos[i].getNota() >= 5) {
-                alumnos[i].mostrarDatos();
-            }
+        int suma = 0;
+        Alumno mejorAlumno = alumnos[0];
 
+        for (int i = 0; i < alumnos.length; i++) {
+            alumnos[i].mostrarDatos();
+            suma = suma + alumnos[i].getNota();
+            if (alumnos[i].getNota() > mejorAlumno.getNota()) {
+                mejorAlumno = alumnos[i];
+            }
+            // if (alumnos[i].getNota() >= 5) {
+            //    alumnos[i].mostrarDatos();
+            // }
         }
+        double media = (double) suma / alumnos.length;
+        System.out.print("\n--------------------------------------------------");
+        System.out.printf("\nMedia: %.2f", media);
+        System.out.println("\nLa nota más alta es de: " + mejorAlumno.getNombre() + " con un " + mejorAlumno.getNota());
     }
 }
