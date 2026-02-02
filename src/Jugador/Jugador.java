acd8b0e7222e0d5165c5fc61689a5adf7faf3e7d
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-02-02 11:46:32 +0100

    Implementada clase Jugador

--- src/Jugador/Jugador.java
+++ src/Jugador/Jugador.java
@@ -1,4 +1,20 @@
 package Jugador;
 
 public class Jugador {
+
+    private String nombre;
+    private int vida;
+
+    public Jugador(String nombre, int vida) {
+        this.nombre = nombre;
+        this.vida = vida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
 }
