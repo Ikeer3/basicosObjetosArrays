2f7036fa7a064632b519eacb4783a84529ed96c3
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-02-02 11:39:34 +0100

    Implementado mejores alumnos y buscar alumno por escaner

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,60 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Alumno[] alumnos = new Alumno[5];
 
-        alumnos[0] = new Alumno("Carlos", 1);
-        alumnos[1] = new Alumno("Iván", 4);
-        alumnos[2] = new Alumno("Iker", 8);
+        alumnos[0] = new Alumno("Carlos", 6);
+        alumnos[1] = new Alumno("Iván", 9);
+        alumnos[2] = new Alumno("Iker", 9);
         alumnos[3] = new Alumno("Dani", 7);
-        alumnos[4] = new Alumno("Isabella", 3);
+        alumnos[4] = new Alumno("Isabella", 5);
 
         int suma = 0;
-        Alumno mejorAlumno = alumnos[0];
+        int mejorAlumno = 0;
 
         for (int i = 0; i < alumnos.length; i++) {
-            alumnos[i].mostrarDatos();
             suma = suma + alumnos[i].getNota();
-            if (alumnos[i].getNota() > mejorAlumno.getNota()) {
-                mejorAlumno = alumnos[i];
+            if (alumnos[i].getNota() >= 5) {
+                alumnos[i].mostrarDatos();
             }
-            // if (alumnos[i].getNota() >= 5) {
-            //    alumnos[i].mostrarDatos();
-            // }
+            if (alumnos[i].getNota() > alumnos[mejorAlumno].getNota()) {
+                mejorAlumno = i;
+            }
+
+        }
+        System.out.print("\n--------------------------------------------------");
+
+        for (int i = 0; i < alumnos.length; i++) {
+            if (alumnos[mejorAlumno].getNota() == alumnos[i].getNota()) {
+                System.out.print("\nLas notas más altas son de: " + alumnos[i].getNombre() + " con un " + alumnos[i].getNota());
+            }
         }
         double media = (double) suma / alumnos.length;
-        System.out.print("\n--------------------------------------------------");
+        System.out.println();
         System.out.printf("\nMedia: %.2f", media);
-        System.out.println("\nLa nota más alta es de: " + mejorAlumno.getNombre() + " con un " + mejorAlumno.getNota());
+
+        Scanner escaner = new Scanner(System.in);
+
+        System.out.print("\n--------------------------------------------------");
+        System.out.print("\nQue nombre quieres buscar?: ");
+        String nombre = escaner.next();
+
+        boolean encontrado = false;
+        int posicion = 0;
+
+        for (int i = 0; i < alumnos.length; i++) {
+            if (alumnos[i].getNombre().equalsIgnoreCase(nombre)) {
+                encontrado = true;
+                posicion = i;
+                break;
+            }
+        }
+        if (encontrado) {
+            System.out.println("Alumno encontrado! Su nota es: " + alumnos[posicion].getNota());
+        } else {
+            System.out.println("Alumno no encontrado");
+        }
     }
 }
